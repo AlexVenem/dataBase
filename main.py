8cb36465ca01ddaa55b71bf036252ffa7bd13734
gurusooo <130143477+gurusooo@users.noreply.github.com>
2023-10-19 23:47:57 +0300

    main skeleton

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import os
 import random
+
+def main ():
+    print('Предлагаю так. f - delete student from db; w - get info about student by name хз;\n'
+          'a - put student into db; e - edit info; ещё хочу сделать штуку чтобы оценки ставить можно было\n'
+          'это будет наша фича; d - display table;\n'
+          )
+    while True:
+        choice=input()
+        if choice == "f":
+
+        elif choice = "w":
+
+        elif choice = "a":
+
+        elif choice = "e":
+
+        elif choice = "d":
+
+        else:
+            print('Something went wrong!')
+
+if __name__ == '__main__':
+    main()
